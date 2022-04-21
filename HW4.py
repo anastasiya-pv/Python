c51b9c9bd2d5b222955ec43b4f6d625c0b71c6b5
anastasiya-pv <nastya.poplevko@gmail.com>
2022-04-21 22:45:44 +0300

    HW_4

---
+++ HW4.py
@@ -0,0 +1,77 @@
+# Python HW 4 Cycles
+# Цилы While
+# Создать переменную count со значением 0
+count = 0
+# Создать переменную range_count со значением 10
+range_count = 10
+# Создать переменную for_count со значением 0
+for_count = 0
+# Создать переменную run  со значением True
+run = True
+#
+# Сделать цикл while который будет работать пока run
+# Тело цикла:
+# 	5.1 Выводить в консоль “Hello Cycle”
+while run:
+    print("Hello Cycle")
+# Сделать цикл while который будет работать пока run
+# Тело цикла:
+# 	6.1 Выводить в консоль (“Step =”, count)
+# 	6.2 Переменной count прибавлять 1 с присвоением.
+while run:
+    print("step =", count)
+    count +=1
+# Сделать цикл while который будет работать пока count < range_count
+# Тело цикла:
+# 	7.1 Выводить в консоль (“Step =”, count)
+# 	7.2 Переменной count прибавлять 1 с присвоением.
+ while count < range_count:
+     print("Step =", count)
+     count +=1
+# Сделать цикл while который будет работать пока count < range_count
+# Тело цикла:
+# 	8.1 Выводить в консоль (“Step =”, count)
+# 	8.2 Переменной count прибавлять 1 с присвоением.
+# 	8.3 Сделать if с условием, если count равен 3 то выводить в консоль (“Step =”, count, ‘If body’)
+ while count < range_count:
+     print("Step =", count)
+     count +=1
+     if count ==3:
+         print("Step =", count, 'if body')
+# Сделать цикл while который будет работать пока run
+# Тело цикла:
+# 	9.1 Выводить в консоль (“Step =”, count)
+# 	9.2 Переменной count прибавлять 1 с присвоением.
+# 	9.2 Сделать if с условием, если count равен range_count то цикл остановится.
+# 	9.3 В теле if вывести в консоль (“STOP”, count)
+ while run:
+     print("Step =", count)
+     count += 1
+     if count == range_count:
+         print("STOP", count)
+         break
+# Цилы For
+# Сделать цикл for c переменной item который будет работать пока счётчик range досчитает от for_count  до range_count
+# Тело цикла:
+# 10.1 Вывести в консоль (‘Step =’, item)
+item = 0
+for item in range(for_count, range_count):
+ print("Step =", item)
+# Сделать цикл for c переменной item который будет работать пока счётчик range досчитает от 0 до 30
+# Тело цикла:
+# 11.1 Вывести в консоль (‘Step =’, item)
+# 11.2 Сделать if с условием, если item равен  5, то вывести в консоль (‘Item =’, item).
+# 11.3 Сделать if с условием, если item равен  10, то вывести в консоль (‘Item =’, item).
+# 11.4 Сделать if с условием, если item меньше 4, то вывести в консоль (‘Item <’, item).
+# 11.5 Сделать if с условием, если item больше или равно 27, то вывести в консоль (‘Item >=’, item).
+item = 0
+for item in range(0, 30):
+    print("Step =", item)
+    if item == 5:
+        print("Item =", item)
+    elif item == 10:
+        print("Item =", item)
+    elif item < 4:
+        print("Item <4 =", item)
+    elif item >= 27:
+        print("Item >=", item)
